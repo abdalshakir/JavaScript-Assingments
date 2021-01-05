276734e93999dabd87517651a997b2058ab93f8f
Abdal Shakir <abdal_shakir7152hotmail.com>
2021-01-05 17:58:51 +0500

    javascript assingments

---
+++ app.js
@@ -0,0 +1,120 @@
+// Chapter 1
+// alert("Hello World")
+
+// alert("Error! Please enter a valid passwrord.")
+
+// alert("Welcom to JS Land... \nHappy Coding! ")
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogues.")
+
+// console.log("Hello... I can run JS through my web browser's console")
+
+// alert("This is my Project")
+
+// Chapter 2
+// var username = "";
+
+// var myName = "Abdal Shakir";
+
+// var message = "Hello World";
+// alert(message);
+
+// var studentName = "Sarmad";
+// alert(studentName);
+// var age = "15 years old";
+// alert(age);
+// var course = "Certified Mobile Application Development"
+// alert(course);
+
+// var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(food);
+
+// var email = "abdal.shakir715715@gmail.com";
+// alert("My email address is" + " " + email);
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I'm trying to learn from the book" + " " + book);
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
+// Chapter 3
+
+// var age = "I'm 15 years old";
+// alert(age);
+
+// var tracking = "You have visited this site 14 times";
+// alert(tracking);
+
+// var birthYear = "1998";
+// document.write("My birth year is" + " " + birthYear + "<br>" + "Data type of my declared variable is number" );
+
+// var visitorName = "<b>Sarmad</b>";
+// var productTitle = "<b>T-Shirts</b>";
+// var quantity = "<b>5</b>";
+// document.write(visitorName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "on XYZ Clothing Store" );
+
+// Chapter 4
+
+// var name = "Sarmad";
+// var age = "15 years old";
+// var grade = "Matric";
+// document.write("My student" + " " + name + " " + "is" + " " + age + " " + "and studied in" + " " + grade);
+
+// var user name = ;
+// var 1user = ;
+// var !user = ;
+// var @user = ;
+// var %user = ;
+// var userName = ;
+// var userName1 = ;
+// var userName$ = ;
+// var user_Name = ;
+
+// var num = "numbers";
+// var letter = "letter";
+// var dollarSigns = "$";
+// var undrerScores = "_";
+// document.write("<h1>Rules for naming JS variables</h1>" + "<br />" + "Variable names can only contain" + " " + num + "," + " " + dollarSigns + " " + "and" + " " + undrerScores + "." + " " + "For example <b>$my_1stVariable</b>" + "<br />" + "Variables must begin with a" + " " + letter + "," + " " + dollarSigns + " " + "and" + " " + undrerScores + "." + " " + "For example"  + " " + "<b>" + dollarSigns + "name" + "," + " " + undrerScores + "name" +"</b>" + " " + "or" + " " + "<b>" + "name" + "</b>" + "." + "<br />" + "Variable names are case sensitive." + "<br />" + "Variable names should not be JS keywords.");
+
+//  Chapter 5
+
+// var numOne = 3;
+// var numTwo = 5;
+// var total = numOne + numTwo;
+// document.write("Sum of" + " " + numOne + " " + "and" + " " + numTwo + " " + "is" + " " + total)
+
+// var numOne = 5;
+// var numTwo = 3;
+// var total = numOne - numTwo;
+// document.write("Subtraction of" + " " + numOne + " " + "and" + " " + numTwo + " " + "is" + " " + total)
+
+// var numOne = 5;
+// var numTwo = 3;
+// var total = numOne * numTwo;
+// document.write("Multiplication of" + " " + numOne + " " + "and" + " " + numTwo + " " + "is" + " " + total)
+
+// var numOne = 6;
+// var numTwo = 3;
+// var total = numOne / numTwo;
+// document.write("Division of" + " " + numOne + " " + "and" + " " + numTwo + " " + "is" + " " + total)
+
+// var numOne = 10;
+// var numTwo = 3;
+// var total = numOne % numTwo;
+// document.write("Modulus of" + " " + numOne + " " + "and" + " " + numTwo + " " + "is" + " " + total)
+
+// var a = "undefined";
+// document.write("Value after variable declaration is" + " " + a + "<br />");
+// var a = 10;
+// document.write("Initial value:" + " " + a + "<br />");
+// document.write("Value after increment is:" + " " + ++a + "<br />");
+// var b = a + 6;
+// document.write("Value after addition is:" + " " + b + "<br />");
+// document.write("Value after decrement is:" + " " + --b + "<br />");
+// document.write("The remainder is:" + " " + [b % 4] );
+
